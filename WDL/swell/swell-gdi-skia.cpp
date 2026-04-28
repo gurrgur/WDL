a27c820f8a68920805ba5ce7a994a841655d9e1b
Marcus Gursch <marcus.gursch@gmail.com>
2026-04-28 20:09:05 +0200

    opinion

--- WDL/swell/swell-gdi-skia.cpp
+++ WDL/swell/swell-gdi-skia.cpp
@@ -568,7 +568,7 @@ void *SWELL_SkiaFontFromFile(const char *path, int index, float pixel_size)
 
   SkFont *font = new SkFont(std::move(tf), pixel_size);
   font->setEdging(SkFont::Edging::kAntiAlias);
-  font->setHinting(SkFontHinting::kSlight);
+  font->setHinting(SkFontHinting::kFull);
   font->setSubpixel(false);
 
   return font;
